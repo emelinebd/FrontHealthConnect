7d223b934ec85c67df9f37bcc67ca835829bbb86
Lucie G <guerrluc@gmail.com>
2024-05-24 14:45:42 +0200

    fix: add push main

--- .husky/check-branch-name.sh
+++ .husky/check-branch-name.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
 branch="$(git symbolic-ref --short HEAD)"
-if ! [[ "$branch" =~ ^(feat|fix|hotfix|doc)/[a-z0-9._-]+$ ]]; then
-  echo "Error: Branch name '$branch' does not follow the convention."
-  echo "Branch names should start with 'feat/', 'fix/', 'hotfix/', or 'doc/' and contain only lowercase letters, numbers, dots, hyphens, and underscores."
-  exit 1
+if [[ "$branch" != "main" ]]; then
+  if ! [[ "$branch" =~ ^(feat|fix|hotfix|doc)/[a-z0-9._-]+$ ]]; then
+    echo "Error: Branch name '$branch' does not follow the convention."
+    echo "Branch names should start with 'feat/', 'fix/', 'hotfix/', or 'doc/' and contain only lowercase letters, numbers, dots, hyphens, and underscores."
+    exit 1
+  fi
 fi
